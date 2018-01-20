24c1b9a61ae0df6f68fc7d73d786bf1210b3f780
faithngetich <faith.ngetich@andela.com>
2018-01-20 11:07:01 +0300

    Remove Statoc Pages controllers

--- app/controllers/statoc_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StatocPagesController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'statoc_pages/home'
-
-  get 'statoc_pages/help'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
